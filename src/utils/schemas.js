1d822320b2ba613b4426148a9636116fc39b3b4e
Lívia Nascimento <liviapnd@gmail.com>
2023-10-10 16:55:17 -0300

    fix: correções nos schemas

--- src/utils/schemas.js
+++ src/utils/schemas.js
@@ -3,11 +3,11 @@ const joi = require("joi");
 
 const schemaUsuario = joi.object({
   nome: joi.string().messages({}),
-  email: joi.email().require().message({
+  email: joi.string().email().required().messages({
     "string.mail": "o campo email precisa ter um formato válido",
     "any.required": "O campo email é obrigatório",
   }),
-  senha: joi.string().min(5).required().message({
+  senha: joi.string().min(5).required().messages({
     "any.required": "O campo senha é obrigatório",
     "string.min": "O campo senha requer 5 digitos",
     "string.empty": "O campo senha é obrigatório",
@@ -15,11 +15,11 @@ const schemaUsuario = joi.object({
 });
 
 const schemaLogin = joi.object({
-  email: joi.email().require().message({
+  email: joi.string().email().required().messages({
     "string.mail": "o campo email precisa ter um formato válido",
     "any.required": "O campo email é obrigatório",
   }),
-  senha: joi.string().min(5).required().message({
+  senha: joi.string().min(5).required().messages({
     "any.required": "O campo senha é obrigatório",
     "string.min": "O campo senha requer 5 digitos",
     "string.empty": "O campo senha é obrigatório",
