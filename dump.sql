ab2904b71acf1d4eb7f5ee121c8ea84e1dc32cc9
Lívia Nascimento <liviapnd@gmail.com>
2023-10-10 17:40:13 -0300

    chore: add dump.sql com insert nas categorias

---
+++ dump.sql
@@ -0,0 +1,11 @@
+INSERT INTO categorias (descricao) 
+VALUES
+('Informática'),
+('Celulares'),
+('Beleza e Perfumaria'),
+('Mercado'),
+('Livros e Papelaria'),
+('Brinquedos'),
+('Moda'),
+('Bebê'),
+('Games')
