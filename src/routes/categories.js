450dded56993fc6a4330627e97ca788aba095c4f
Lívia Nascimento <liviapnd@gmail.com>
2023-10-24 19:25:42 -0300

    fix: adicionando middleware de validação de categoria

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
 const { listCategories } = require("../controllers/categories");
+const { verifyCategoryExist } = require("../middlewares/verify");
 
 const categoryRoutes = express();
 
-categoryRoutes.get("/categoria", listCategories);
+categoryRoutes.get("/categoria", verifyCategoryExist, listCategories);
 
 module.exports = categoryRoutes;
