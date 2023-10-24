ed2c4be2118b3253627bd98f9022796256c3842f
Lívia Nascimento <liviapnd@gmail.com>
2023-10-23 23:19:49 -0300

    refactor: adicionando coluna produto_imagem na tabela produtos

--- src/db/migrations/20231018183416_create_produtos_table.js
+++ src/db/migrations/20231018183416_create_produtos_table.js
@@ -1,14 +1,16 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("produtos", function (table) {
-    table.increments("id").primary(),
+    return knex.schema.createTable("produtos", function (table) {
+      table.increments("id").primary(),
       table.string("descricao", 255).notNullable(),
-      table.integer("quantidade_estoque").notNullable().unsigned();
-    table.float("valor", 100).notNullable().unsigned();
-    table.integer("categoria_id").notNullable().unsigned();
-    table.foreign("categoria_id").references("categorias.id");
-  });
-};
-
-exports.down = function (knex) {
-  return knex.schema.dropTable("produtos");
-};
+      table.integer("quantidade_estoque").notNullable().unsigned(),
+      table.integer("valor").notNullable().unsigned(),
+      table.integer("categoria_id").notNullable().unsigned(),
+      table.foreign("categoria_id").references("categorias.id"),
+      table.text("produto_imagem")
+    });
+  };
+  
+  exports.down = function (knex) {
+    return knex.schema.dropTable("produtos");
+  };
+  
