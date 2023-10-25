6a7e86b8541e7d2995f31049433a08f6cf1f795b
Lívia Nascimento <liviapnd@gmail.com>
2023-10-25 11:45:48 -0300

    fix: correção da referência para chave estrageira na migration pedido_produtos

--- src/db/migrations/20231024022120_create_pedido_produtos_table.js
+++ src/db/migrations/20231024022120_create_pedido_produtos_table.js
@@ -1,21 +1,25 @@
 exports.up = function(knex) {
     return knex.schema.createTable("pedido_produtos", function (table) {
         table.increments("id").primary()
-        table.integer("pedido_id").notNullable().unsigned()
+        table.integer("pedido_id")
+        .notNullable()
+        .unsigned()
         .references("id")
         .inTable("pedidos")
         .onDelete("CASCADE")
         .onUpdate("CASCADE");
-        table.integer("produto_id").notNullable().unsigned()
+        table.integer("produto_id")
+        .notNullable()
+        .unsigned()
         .references("id")
-        .inTable("clientes")
+        .inTable("produtos")
         .onDelete("CASCADE")
-        .onUpdate("CASCADE")
+        .onUpdate("CASCADE");
         table.integer("valor_produto")
         table.integer("quantidade_produto").notNullable()
       });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("pedido_produtos");
 };
